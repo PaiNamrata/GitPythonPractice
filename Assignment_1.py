8b65aa096d416c16a40afe2a1b8118b020214b66
NAMRATA PAI <namrata180696@gmail.com>
2026-02-25 19:20:40 +0530

    Add files via upload

---
+++ Assignment_1.py
@@ -0,0 +1,33 @@
+#TASK 1:  Perform Basic Mathematical Operations
+'''num1=int(input("enter first number:" ))
+num2=int(input("enter second number:" ))
+print(f"Addition = {num1+num2} " )
+if num2 > num1 :
+    print(f"Substraction = {num2-num1}")
+else :
+    print(f"Substraction = {num1-num2}")
+if num2 !=0 or num1 !=0:
+    print(f"Multiplication = {num2*num1}")
+else :
+    print(f"Multiplication is 0")
+
+if num2 !=0 & num2 >= num1:
+    print(f"Division = {num1/num2}")
+elif num2 ==0 :
+    print(f"Division is not valid")
+else:
+    print(f"Division = {num1 / num2}") '''
+
+#TASK 2:  Create a Personalized Greeting
+first_name = input("enter your First Name:")
+last_name = input("enter your Last Name:")
+
+print(f"Hello, {first_name} {last_name}! Welcome to Pyhon Programming.")
+
+
+
+
+
+
+
+
